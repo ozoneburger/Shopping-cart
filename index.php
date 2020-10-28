1f46019060e7226c75e1cbb2a2f91330ce3af376
unknown <robertmaligro@gmail.com>
2020-10-28 20:27:36 +1300

    changed shoppingcart.php to index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+            // ######## please do not alter the following code ########
+            $products = [
+            [ "name" => "Sledgehammer", "price" => 125.75 ],
+            [ "name" => "Axe", "price" => 190.50 ],
+            [ "name" => "Bandsaw", "price" => 562.131 ],
+            [ "name" => "Chisel", "price" => 12.9 ],
+            [ "name" => "Hacksaw", "price" => 18.45 ],
+            ];
+            // ########################################################
+            ?>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title>Mitre20 Mega</title>
+</head>
+
+<body>
+    <center><h1>Mitre20 Mega</h1></center>
+    <form method="post" action="shoppingcart.php">
+      <label id="productlabel">Products: </label><br>
+      <button></button>
+    </form>
+    <p id="postformButton"><a href="index.html" class="linkbutton">Return to homepage</a></p>
+</body>
+</html>
